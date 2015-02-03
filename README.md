b394ffd278f88349e3c60f4c5913e0641714a04b
sehili <sehili@chm6129.(none)>
2015-02-03 19:18:42 +0100

    Add speech-rec.sh and README.md

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-../../projects/gspeech-rec/README.md
+# gspeech-rec
+
+### A demonstration of Google's Speech Recognition API v2.
+
+**speech-rec.sh** is a bash script that sends a flac audio file to Google for speech recognition and prints the N-best returned hypotheses.
+
+More interestingly, if you have **parecord** and **timeout** installed on your machine, you can use the script interactively to record and utterance and get its transcription.
+
+# Some examples
+
+### Recognize speech in audio file
+    ./speech-rec.sh -i record.flac --rate 16000
+
+### Use another language than en-US (default):
+French
+
+    ./speech-rec.sh -i record.flac --rate 16000 --language fr-FR
+
+Spanish
+
+    ./speech-rec.sh -i record.flac -r 16000 -l es-ES
+
+### Simply say something and get (hopefully) what you've said
+    ./speech-rec.sh
+   
+### Talk for 7 seconds (default: 3)
+    ./speech-rec.sh -d 7
+   
+The scripts write audio data into an audio file named **record.flac** and doesn't delete it afterwards so you can listen to what you said.
+To play the audio file type:
+
+    paplay record.flac
+   
+# Requirements
+### Google API Key
+The included key is only to be used for test purposes and may be disabled anytime. You should generate your own API key (follow this link:
+http://www.chromium.org/developers/how-tos/api-keys )
+
+#### parecord
+To record and play audio data
+
+#### timeout
+To stop **parecord** after the required duration
+
+#### wget
+To send data to server
+
+
+# Python API
+https://github.com/Uberi/speech_recognition/
+
+# Author
+amsehili <amine.sehili@gmail.com> ([Mohamed El] Amine Sehili)
+
+# License
+Copyright (C) Amine Sehili 2015.
+
+This program is available under the GNU GENERAL PUBLIC LICENSE Version 2.
+
+Source code on GitHub: https://github.com/amsehili/gspeech-rec
+
+
--- speech-rec.sh
+++ speech-rec.sh
@@ -1,1 +1,111 @@
-../../projects/gspeech-rec/speech-rec.sh
+#!/bin/bash
+ 
+# Usage info
+show_help() {
+cat << EOF
+  Usage: ${0##*/} [-hv] [-i INFILE] [-d DURATION] [-r RATE] [-l LANGUAGE] [-k KEY]
+  
+  Record an utterance and send audio data to Google for speech recognition.
+  
+       -h|--help               display this help and exit
+       -i|--input     INFILE   use INFILE instead of recording a stream with parecord.
+       -d|--duration  FLOAT    recoding duration in seconds (Default: 3).
+       -l|--language  STRING   set transcription language (Default: en_US).
+                               Other languages: fr-FR, de-DE, es-ES, ...
+       -r|--rate      INTEGER  Sampling rate of recorded data (Default: 16000).
+                               If -i|--input is used, the sampling rate must be supplied by the user.
+       -k|--key       STRING   Google Speech Recognition Key.
+                  
+EOF
+}
+ 
+DURATION=3
+LANGUAGE=en-US
+# Please replace this wih your own key
+KEY=AIzaSyAcalCzUvPmmJ7CZBFOEWx2Z1ZSn4Vs1gg
+ 
+# parse parameters
+while [[ $# -ge 1 ]]
+do
+   key="$1"
+   case $key in
+       -h|--help)
+       show_help
+       exit 0
+       ;;
+       -i|--input)
+       INFILE="$2"
+       shift
+       ;;
+       -d|--duration)
+       DURATION="$2"
+       shift
+       ;;
+       -r|--rate)
+       SRATE=$2
+       shift
+       ;;
+       -l|--language)
+       LANGUAGE="$2"
+       shift
+       ;;
+       -k|--key)
+       KEY="$2"
+       shift
+       ;;
+       *)
+       echo "Unknown parameter '$key'. Type $0 -h for more information."
+       exit 1
+       ;;
+   esac
+   shift
+done
+ 
+if [[ ! "$DURATION" ]]
+   then
+     echo "ERROR: empty or invalid value for duration."
+     exit 1
+fi
+ 
+if [[ ! "$LANGUAGE" ]]
+   then
+     echo "ERROR: empty value for language."
+     exit 1
+fi
+ 
+if [[ ! "$INFILE" ]]
+   then
+      INFILE=record.flac
+      if  [[ ! "$SRATE" ]]
+         then
+            SRATE=16000
+      fi
+      echo "Say something..."
+      echo ""
+      timeout $DURATION parecord $INFILE --file-format=flac --rate=$SRATE
+ 
+else
+      if  [[ ! "$SRATE" ]]
+      then
+           >&2 echo "ERROR: no sampling rate specified for input file."
+           exit 1
+      fi
+ 
+      echo "Try to recognize speech from file $INFILE"
+      echo ""
+fi
+ 
+RESULT=`wget -q --post-file $INFILE --header="Content-Type: audio/x-flac; rate=$SRATE" -O - "http://www.google.com/speech-api/v2/recognize?client=chromium&lang=$LANGUAGE&key=$KEY"`
+ 
+FILTERED=`echo "$RESULT" | grep "transcript.*}" | sed 's/,/\n/g;s/[{,},"]//g;s/\[//g;s/\]//g;s/:/: /g' | grep -o -i -e "transcript.*" -e "confidence:.*"`
+ 
+if [[ ! "$FILTERED" ]]
+  then
+     >&2 echo "Unable to recognize speech in audio data"
+else
+    echo "Recognition result:"
+    echo ""
+    echo "$FILTERED"
+fi
+ 
+exit 0
